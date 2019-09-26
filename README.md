c49fe482df99fe0d497638c44a608aaacc485f00
simmol <fonix.simmol@gmail.com>
2019-09-26 08:21:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qibx2v-1w4vth
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qibx2v-1w4vth)
---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent implements OnInit {
+  cart_count;
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  ngOnInit() {
+    this.cart_count = this.cartService.getItems().length
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
